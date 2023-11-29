40b7563f7d2b194d13e3e36d8d55a38cb2948d18
Oranty <thinhnguyenphuc51@gmail.com>
2023-11-30 01:33:49 +0700

    Create Model Latop and Cart in Project LaptopStoreApi

---
+++ LaptopStoreApi/Models/ApplicationLaptopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace LaptopStoreApi.Models
+{
+    public class ApplicationLaptopDbContext : DbContext
+    {
+        public ApplicationLaptopDbContext(DbContextOptions<ApplicationLaptopDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ LaptopStoreApi/Models/Laptop.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaptopStoreApi.Models
+{
+    [Table("Laptops")]
+    public class Laptop
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]  
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        [Required]
+        public double Type { get; set; }
+        [Required]
+        public string Color { get; set; } = string.Empty;
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Status { get; set; } = string.Empty;
+        [Required]
+        public string Category { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
