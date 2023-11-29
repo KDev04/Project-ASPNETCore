ace9a5a7728b273796611f1c382f784969851e03
Oranty <thinhnguyenphuc51@gmail.com>
2023-11-30 00:54:58 +0700

    add package Microsoft.EntityFrameworkCore.Design and package Microsoft.EntityFrameworkCore.SqlServer

---
+++ LaptopStore/Controllers/LaptopController.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LaptopStore.Models;
+namespace LaptopStore.Controllers
+{
+    public class LaptopController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Detail() => View();
+    }
+}
