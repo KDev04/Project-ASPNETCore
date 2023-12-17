35eb1783efa81cc3e3d23a8c749ed911cbb01a5c
khanh <nguyenkhanhan79@gmail.com>
2023-12-17 08:36:25 +0700

    xử lí warning null

--- LaptopStore/Controllers/LaptopController.cs
+++ LaptopStore/Controllers/LaptopController.cs
@@ -33,60 +33,60 @@ namespace LaptopStore.Controllers
         }
         public IActionResult Detail() => View();
 
-        public async Task<IActionResult> SaveProduct(Laptop model)
-        {
-            if (ModelState.IsValid)
-            {
-                using (var httpClient = new HttpClient())
-                {
-                    try
-                    {
-                        using (var formData = new MultipartFormDataContent())
-                        {
-                            formData.Add(new StringContent(model.TenLaptop), "TenLaptop");
-                            formData.Add(new StringContent(model.Gia.ToString()), "Gia");
-                            formData.Add(new StringContent(model.GiamGia.ToString()), "GiamGia");
-                            formData.Add(new StringContent(model.LoaiManHinh.ToString()), "LoaiManHinh");
-                            formData.Add(new StringContent(model.Mau), "Mau");
-                            formData.Add(new StringContent(model.NamSanXuat.ToString()), "NamSanXuat");
-                            formData.Add(new StringContent(model.Mota), "Mota");
-                            formData.Add(new StringContent(model.CategoryId.ToString()), "CategoryId");
+        // public async Task<IActionResult> SaveProduct(Laptop model)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         using (var httpClient = new HttpClient())
+        //         {
+        //             try
+        //             {
+        //                 using (var formData = new MultipartFormDataContent())
+        //                 {
+        //                     formData.Add(new StringContent(model.TenLaptop), "TenLaptop");
+        //                     formData.Add(new StringContent(model.Gia.ToString()), "Gia");
+        //                     formData.Add(new StringContent(model.GiamGia.ToString()), "GiamGia");
+        //                     formData.Add(new StringContent(model.LoaiManHinh.ToString()), "LoaiManHinh");
+        //                     formData.Add(new StringContent(model.Mau), "Mau");
+        //                     formData.Add(new StringContent(model.NamSanXuat.ToString()), "NamSanXuat");
+        //                     formData.Add(new StringContent(model.Mota), "Mota");
+        //                     formData.Add(new StringContent(model.CategoryId.ToString()), "CategoryId");
 
-                            if (model.Image != null && model.Image.Length > 0)
-                            {
-                                using (var streamContent = new StreamContent(model.Image.OpenReadStream()))
-                                {
-                                    formData.Add(streamContent, "Image", model.Image.FileName);
+        //                     if (model.Image != null && model.Image.Length > 0)
+        //                     {
+        //                         using (var streamContent = new StreamContent(model.Image.OpenReadStream()))
+        //                         {
+        //                             formData.Add(streamContent, "Image", model.Image.FileName);
 
-                                    var response = await httpClient.PostAsync("http://localhost:4000/api/Laptop/Add", formData);
+        //                             var response = await httpClient.PostAsync("http://localhost:4000/api/Laptop/Add", formData);
 
-                                    if (response.IsSuccessStatusCode)
-                                    {
-                                        // Xử lý khi tạo laptop thành công
-                                        return Redirect("/Laptop/Index");
-                                    }
-                                    else
-                                    {
-                                        // Xử lý khi có lỗi từ API
-                                        ModelState.AddModelError("", "An error occurred while creating the laptop.");
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                ModelState.AddModelError("", "No image file found");
-                            }
-                        }
-                    }
-                    catch
-                    {
-                        ModelState.AddModelError("", "An error occurred");
-                    }
-                }
-            }
+        //                             if (response.IsSuccessStatusCode)
+        //                             {
+        //                                 // Xử lý khi tạo laptop thành công
+        //                                 return Redirect("/Laptop/Index");
+        //                             }
+        //                             else
+        //                             {
+        //                                 // Xử lý khi có lỗi từ API
+        //                                 ModelState.AddModelError("", "An error occurred while creating the laptop.");
+        //                             }
+        //                         }
+        //                     }
+        //                     else
+        //                     {
+        //                         ModelState.AddModelError("", "No image file found");
+        //                     }
+        //                 }
+        //             }
+        //             catch
+        //             {
+        //                 ModelState.AddModelError("", "An error occurred");
+        //             }
+        //         }
+        //     }
 
-            return Redirect("/Laptop/Index");
-        }
+        //     return Redirect("/Laptop/Index");
+        // }
         public IActionResult Create()
         {
             return View();
