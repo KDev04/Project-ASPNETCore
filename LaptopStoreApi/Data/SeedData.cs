ce0748ca9ddcab7902cbca06b9197bfea3b58192
Oranty <thinhnguyenphuc51@gmail.com>
2023-12-18 20:33:02 +0700

    chua lam gi het

--- LaptopStoreApi/Data/SeedData.cs
+++ LaptopStoreApi/Data/SeedData.cs
@@ -89,6 +89,156 @@ namespace LaptopStoreApi.Data
                         CreateDate = DateTime.Now,
                         LastModifiedDate = DateTime.Now,
                         CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 3",
+                        Gia = 1300,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "Leveno",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 3",
+                        ImgPath = "/images/laptop3.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 4",
+                        Gia = 1400,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "Acer",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 2",
+                        ImgPath = "/images/laptop4.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 5",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 5",
+                        ImgPath = "/images/laptop5.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 6",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 6",
+                        ImgPath = "/images/laptop6.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 7",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 7",
+                        ImgPath = "/images/laptop7.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 8.png",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 8",
+                        ImgPath = "/images/laptop8.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 9",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 2",
+                        ImgPath = "/images/laptop9.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 10",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 10",
+                        ImgPath = "/images/laptop10.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 11",
+                        Gia = 1200,
+                        GiamGia = 5,
+                        LoaiManHinh = 14,
+                        Hangsx = "HP",
+                        Mau = "Bạc",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 11",
+                        ImgPath = "/images/laptop11.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
+                    },
+                    new Laptop
+                    {
+                        TenLaptop = "Laptop 12",
+                        Gia = 1900,
+                        GiamGia = 5,
+                        LoaiManHinh = 17,
+                        Hangsx = "GIGABYTE",
+                        Mau = "Đen",
+                        NamSanXuat = 2021,
+                        Mota = "Laptop mô tả 12",
+                        ImgPath = "/images/laptop12.png",
+                        CreateDate = DateTime.Now,
+                        LastModifiedDate = DateTime.Now,
+                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
                     });
                 context.SaveChanges();
             }
--- LaptopStoreApi/Services/LaptopRepository.cs
+++ LaptopStoreApi/Services/LaptopRepository.cs
@@ -122,111 +122,111 @@ namespace LaptopStoreApi.Services
             else return null;
         }*/
         public async Task<List<Laptop>> GetAll()
+    {
+        var laptops = await _context.Laptops.ToListAsync();
+        return laptops;
+    }
+
+    public async Task<Laptop> GetById(int id)
+    {
+        var laptop = await _context.Laptops.FirstOrDefaultAsync(l => l.MaLaptop == id);
+        if (laptop != null)
         {
-            var laptops = await _context.Laptops.ToListAsync();
-            return laptops;
-        }
-
-        public async Task<Laptop> GetById(int id)
-        {
-            var laptop = await _context.Laptops.FirstOrDefaultAsync(l => l.MaLaptop == id);
-            if (laptop != null)
-            {
-                return laptop;
-            }
-            return null;
-        }
-
-        public async Task<Laptop> Add(LaptopModel model)
-        {
-            if (model.Image == null || model.Image.Length == 0)
-            {
-                // Xử lý khi không có tệp hình ảnh được gửi lên
-                // Ví dụ: trả về lỗi hoặc thông báo không có tệp hình ảnh
-                throw new Exception("Không có tệp hình ảnh được gửi lên.");
-            }
-
-            string imgFileName = Guid.NewGuid().ToString() + Path.GetExtension(model?.Image?.FileName);
-            string imgFolderPath = Path.Combine("wwwroot/Image"); // Thư mục "wwwroot/Image"
-            string imgFilePath = Path.Combine(imgFolderPath, imgFileName);
-
-            if (!Directory.Exists(imgFolderPath))
-            {
-                Directory.CreateDirectory(imgFolderPath);
-            }
-
-            using (var stream = new FileStream(imgFilePath, FileMode.Create))
-            {
-                await model?.Image?.CopyToAsync(stream);
-            }
-
-            var laptop = new Laptop
-            {
-                TenLaptop = model?.TenLaptop ?? "",
-                Gia = model.Gia,
-                GiamGia = model.GiamGia,
-                Mau = model.Mau,
-                LoaiManHinh = model.LoaiManHinh,
-                NamSanXuat = model.NamSanXuat,
-                Mota = model.Mota,
-                CategoryId = model.CategoryId,
-                CreateDate = DateTime.Now,
-                LastModifiedDate = DateTime.Now,
-                ImgPath = "Image/" + imgFileName // Đường dẫn tương đối
-            };
-
-            _context.Laptops.Add(laptop);
-            await _context.SaveChangesAsync();
             return laptop;
         }
+        return null;
+    }
 
-        public async Task Update(LaptopModel model)
+    public async Task<Laptop> Add(LaptopModel model)
+    {
+        if (model.Image == null || model.Image.Length == 0)
         {
-            string imgFileName = Guid.NewGuid().ToString() + Path.GetExtension(model.Image.FileName);
-            string imgFolderPath = Path.Combine("wwwroot", "Image"); // Thư mục "wwwroot/Image"
-            string imgFilePath = Path.Combine(imgFolderPath, imgFileName);
-
-            if (!Directory.Exists(imgFolderPath))
-            {
-                Directory.CreateDirectory(imgFolderPath);
-            }
-
-            using (var stream = new FileStream(imgFilePath, FileMode.Create))
-            {
-                await model.Image.CopyToAsync(stream);
-            }
-
-            var laptop = await _context.Laptops.FirstOrDefaultAsync(l => l.MaLaptop == model.MaLaptop);
-            if (laptop != null)
-            {
-                laptop.TenLaptop = model.TenLaptop;
-                laptop.Gia = model.Gia;
-                laptop.GiamGia = model.GiamGia;
-                laptop.Mau = model.Mau;
-                laptop.LoaiManHinh = model.LoaiManHinh;
-                laptop.NamSanXuat = model.NamSanXuat;
-                laptop.Mota = model.Mota;
-                laptop.CategoryId = model.CategoryId;
-                laptop.ImgPath = "Image/" + imgFileName;
-                laptop.LastModifiedDate = DateTime.Now;
-                await _context.SaveChangesAsync();
-            }
+            // Xử lý khi không có tệp hình ảnh được gửi lên
+            // Ví dụ: trả về lỗi hoặc thông báo không có tệp hình ảnh
+            throw new Exception("Không có tệp hình ảnh được gửi lên.");
         }
 
-        public async Task Delete(int id)
+        string imgFileName = Guid.NewGuid().ToString() + Path.GetExtension(model?.Image?.FileName);
+        string imgFolderPath = Path.Combine("wwwroot/Image"); // Thư mục "wwwroot/Image"
+        string imgFilePath = Path.Combine(imgFolderPath, imgFileName);
+
+        if (!Directory.Exists(imgFolderPath))
         {
-            var laptop = await _context.Laptops.FirstOrDefaultAsync(lp => lp.MaLaptop == id);
-            if (laptop != null)
-            {
-                _context.Laptops.Remove(laptop);
-                await _context.SaveChangesAsync();
-            }
+            Directory.CreateDirectory(imgFolderPath);
         }
 
-        public async Task<List<Laptop>> Search(string keyword)
+        using (var stream = new FileStream(imgFilePath, FileMode.Create))
         {
-            var searchResult = await _context.Laptops.Where(l => l.TenLaptop.Contains(keyword)).ToListAsync();
-            return searchResult;
+            await model?.Image?.CopyToAsync(stream);
         }
+
+        var laptop = new Laptop
+        {
+            TenLaptop = model?.TenLaptop ?? "",
+            Gia = model.Gia,
+            GiamGia = model.GiamGia,
+            Mau = model.Mau,
+            LoaiManHinh = model.LoaiManHinh,
+            NamSanXuat = model.NamSanXuat,
+            Mota = model.Mota,
+            CategoryId = model.CategoryId,
+            CreateDate = DateTime.Now,
+            LastModifiedDate = DateTime.Now,
+            ImgPath = "Image/" + imgFileName // Đường dẫn tương đối
+        };
+
+        _context.Laptops.Add(laptop);
+        await _context.SaveChangesAsync();
+        return laptop;
+    }
+
+    public async Task Update(LaptopModel model)
+    {
+        string imgFileName = Guid.NewGuid().ToString() + Path.GetExtension(model.Image.FileName);
+        string imgFolderPath = Path.Combine("wwwroot", "Image"); // Thư mục "wwwroot/Image"
+        string imgFilePath = Path.Combine(imgFolderPath, imgFileName);
+
+        if (!Directory.Exists(imgFolderPath))
+        {
+            Directory.CreateDirectory(imgFolderPath);
+        }
+
+        using (var stream = new FileStream(imgFilePath, FileMode.Create))
+        {
+            await model.Image.CopyToAsync(stream);
+        }
+
+        var laptop = await _context.Laptops.FirstOrDefaultAsync(l => l.MaLaptop == model.MaLaptop);
+        if (laptop != null)
+        {
+            laptop.TenLaptop = model.TenLaptop;
+            laptop.Gia = model.Gia;
+            laptop.GiamGia = model.GiamGia;
+            laptop.Mau = model.Mau;
+            laptop.LoaiManHinh = model.LoaiManHinh;
+            laptop.NamSanXuat = model.NamSanXuat;
+            laptop.Mota = model.Mota;
+            laptop.CategoryId = model.CategoryId;
+            laptop.ImgPath = "Image/" + imgFileName;
+            laptop.LastModifiedDate = DateTime.Now;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task Delete(int id)
+    {
+        var laptop = await _context.Laptops.FirstOrDefaultAsync(lp => lp.MaLaptop == id);
+        if (laptop != null)
+        {
+            _context.Laptops.Remove(laptop);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<Laptop>> Search(string keyword)
+    {
+        var searchResult = await _context.Laptops.Where(l => l.TenLaptop.Contains(keyword)).ToListAsync();
+        return searchResult;
+    }
     }
 }
