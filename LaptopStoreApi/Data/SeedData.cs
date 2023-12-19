69330252ecda5fe1df235b1652d05d1803e9b4cc
khanh <nguyenkhanhan79@gmail.com>
2023-12-19 07:54:30 +0700

    commit to pull

--- LaptopStoreApi/Data/SeedData.cs
+++ LaptopStoreApi/Data/SeedData.cs
@@ -23,42 +23,6 @@ namespace LaptopStoreApi.Data
             }
             if (!context.Laptops.Any())
             {
-                // Thêm dữ liệu mẫu cho bảng Laptops
-                /*var laptops = new[]
-                {
-                    new Laptop
-                    {
-                        TenLaptop = "Laptop 1",
-                        Gia = 1000,
-                        GiamGia = 10,
-                        LoaiManHinh = 15.6,
-                        Hangsx = "Dell",
-                        Mau = "Đen",
-                        NamSanXuat = 2022,
-                        Mota = "Laptop mô tả 1",
-                        ImgPath = "/images/laptop1.jpg",
-                        CreateDate = DateTime.Now,
-                        LastModifiedDate = DateTime.Now,
-                        CategoryId = 1 // Thay 1 bằng Id thực tế của Category
-                    },
-                    new Laptop
-                    {
-                        TenLaptop = "Laptop 2",
-                        Gia = 1200,
-                        GiamGia = 5,
-                        LoaiManHinh = 14,
-                        Hangsx = "HP",
-                        Mau = "Bạc",
-                        NamSanXuat = 2021,
-                        Mota = "Laptop mô tả 2",
-                        ImgPath = "/images/laptop2.jpg",
-                        CreateDate = DateTime.Now,
-                        LastModifiedDate = DateTime.Now,
-                        CategoryId = 2 // Thay 2 bằng Id thực tế của Category
-                    },
-                    // Thêm các dòng dữ liệu khác tương tự
-                };*/
-
                 context.Laptops.AddRange(
                     new Laptop
                     {
