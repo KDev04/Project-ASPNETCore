104278c422691bb7da46db90e3c07197afcb854d
khanh <nguyenkhanhan79@gmail.com>
2023-12-21 17:42:01 +0700

    merge

--- LaptopStoreApi/Services/ILaptopRepository.cs
+++ LaptopStoreApi/Services/ILaptopRepository.cs
@@ -1,15 +1,15 @@
-﻿using LaptopStoreApi.Data;
-using LaptopStoreApi.Models;
-namespace LaptopStoreApi.Services
-{
-    public interface ILaptopRepository
-    {
-        Task<List<Laptop>> GetAll();
-        List<Laptop> Filter(string name, decimal? from, decimal? to, string sortBy, int page =1);
-        Task<Laptop> GetById(int id);
-        Task<Laptop> Add(LaptopModel model);
-        Task Update(LaptopModel model);
-        Task Delete(int id);
-        Task<List<Laptop>> Search(string keyword);
-    }
-}
+﻿// using LaptopStoreApi.Data;
+// using LaptopStoreApi.Models;
+// namespace LaptopStoreApi.Services
+// {
+//     public interface ILaptopRepository
+//     {
+//         Task<List<Laptop>> GetAll();
+//         List<Laptop> Filter(string name, decimal? from, decimal? to, string sortBy, int page =1);
+//         Task<Laptop> GetById(int id);
+//         Task<Laptop> Add(LaptopModel model);
+//         Task Update(LaptopModel model);
+//         Task Delete(int id);
+//         Task<List<Laptop>> Search(string keyword);
+//     }
+// }
--- LaptopStoreApi/obj/Debug/net7.0/LaptopStoreApi.AssemblyInfo.cs
+++ LaptopStoreApi/obj/Debug/net7.0/LaptopStoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9501ce7cb26a69354479d79ce38e8ccff6a14fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
