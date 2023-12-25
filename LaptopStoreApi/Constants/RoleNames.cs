400b6d387232f9bccad38cdc947cc7ae1264ec65
khanh <nguyenkhanhan79@gmail.com>
2023-12-25 19:58:26 +0700

    đăng kí , sửa database

---
+++ LaptopStoreApi/Constants/RoleNames.cs
@@ -0,0 +1,9 @@
+﻿namespace LaptopStoreApi.Constants
+{
+    public static class RoleNames
+    {
+        public const string PowerUser = "PowerUser";
+        public const string Moderator = "Moderator";
+        public const string Administrator = "Administrator";
+    }
+}
---
+++ LaptopStoreApi/Controllers/SeedController.cs
@@ -0,0 +1,88 @@
+﻿using LaptopStoreApi.Constants;
+using LaptopStoreApi.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopStoreApi.Controllers
+{
+    /*    [Authorize(Roles = RoleNames.Administrator)]*/
+    [Authorize]
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        private readonly ILogger<SeedController> _logger;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly UserManager<User> _userManager;
+
+        public SeedController(
+            ApiDbContext context,
+            IWebHostEnvironment env,
+            ILogger<SeedController> logger,
+            RoleManager<IdentityRole> roleManager,
+            UserManager<User> userManager)
+        {
+            _context = context;
+            _env = env;
+            _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        [HttpPost]
+        [ResponseCache(NoStore = true)]
+        public async Task<IActionResult> AuthData()
+        {
+            int rolesCreated = 0;
+            int usersAddedToRoles = 0;
+
+            if (!await _roleManager.RoleExistsAsync(RoleNames.Moderator))
+            {
+                await _roleManager.CreateAsync(
+                    new IdentityRole(RoleNames.Moderator));
+                rolesCreated++;
+            }
+            if (!await _roleManager.RoleExistsAsync(RoleNames.Administrator))
+            {
+                await _roleManager.CreateAsync(
+                    new IdentityRole(RoleNames.Administrator));
+                rolesCreated++;
+            }
+
+            var testModerator = await _userManager
+                .FindByNameAsync("TestModerator");
+            if (testModerator != null
+                && !await _userManager.IsInRoleAsync(
+                    testModerator, RoleNames.Moderator))
+            {
+                await _userManager.AddToRoleAsync(testModerator, RoleNames.Moderator);
+                usersAddedToRoles++;
+            }
+
+            var testAdministrator = await _userManager
+                .FindByNameAsync("TestAdministrator");
+            if (testAdministrator != null
+                && !await _userManager.IsInRoleAsync(
+                    testAdministrator, RoleNames.Administrator))
+            {
+                await _userManager.AddToRoleAsync(
+                    testAdministrator, RoleNames.Moderator);
+                await _userManager.AddToRoleAsync(
+                    testAdministrator, RoleNames.Administrator);
+                usersAddedToRoles++;
+            }
+
+            return new JsonResult(new
+            {
+                RolesCreated = rolesCreated,
+                UsersAddedToRoles = usersAddedToRoles
+            });
+        }
+    }
+}
