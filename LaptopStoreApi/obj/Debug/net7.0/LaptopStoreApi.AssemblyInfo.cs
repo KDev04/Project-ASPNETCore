ee86dd86168a407529e07a27f75ae16fc2faca8b
khanh <nguyenkhanhan79@gmail.com>
2023-12-25 13:25:58 +0700

    merge

--- LaptopStoreApi/obj/Debug/net7.0/LaptopStoreApi.AssemblyInfo.cs
+++ LaptopStoreApi/obj/Debug/net7.0/LaptopStoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec67b32e32a1fd58f45f2384e09002cde2627cbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaptopStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
