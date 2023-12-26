97319aae591926e6e0e0179ea6fdd29a6584708b
Oranty <thinhnguyenphuc51@gmail.com>
2023-12-27 00:19:13 +0700

    Tạo 3 Account (Password): Admin (Admin123), Moderator (Mod123), Client (Client123) bằng Seed/CreateAccount

--- LaptopStoreApi/Controllers/SeedController.cs
+++ LaptopStoreApi/Controllers/SeedController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LaptopStoreApi.Controllers
 {
     /*    [Authorize(Roles = RoleNames.Administrator)]*/
-    [Authorize]
+    
     [Route("[controller]/[action]")]
     [ApiController]
     public class SeedController : ControllerBase
@@ -35,6 +35,7 @@ namespace LaptopStoreApi.Controllers
             _roleManager = roleManager;
             _userManager = userManager;
         }
+        [Authorize]
         [HttpPost]
         [ResponseCache(NoStore = true)]
         public async Task<IActionResult> AuthData()
@@ -56,7 +57,7 @@ namespace LaptopStoreApi.Controllers
             }
 
             var testModerator = await _userManager
-                .FindByNameAsync("TestModerator");
+                .FindByNameAsync("Moderator");
             if (testModerator != null
                 && !await _userManager.IsInRoleAsync(
                     testModerator, RoleNames.Moderator))
@@ -66,7 +67,7 @@ namespace LaptopStoreApi.Controllers
             }
 
             var testAdministrator = await _userManager
-                .FindByNameAsync("TestAdministrator");
+                .FindByNameAsync("Admin");
             if (testAdministrator != null
                 && !await _userManager.IsInRoleAsync(
                     testAdministrator, RoleNames.Administrator))
@@ -83,6 +84,92 @@ namespace LaptopStoreApi.Controllers
                 RolesCreated = rolesCreated,
                 UsersAddedToRoles = usersAddedToRoles
             });
+        }
+
+        [HttpPost]
+        [ResponseCache(NoStore = true)]
+        public async Task<IActionResult> CreateAccount()
+        {
+            int Acc = 0;
+            string testAd = "Admin chưa được khởi tạo.";
+            string testMod = "Moderator chưa được khởi tạo";
+            string testCli = "Client chưa được khởi tạo";
+
+            var testAdministrator = await _userManager.FindByNameAsync("Admin");
+            if (testAdministrator != null) 
+            {
+                Acc++; 
+                testAd = "Admin đã được khởi tạo."; 
+            } 
+            else
+            {
+                var Admin = new User();
+                Admin.UserName = "Admin";
+                Admin.Email = "Admin@gmail.com";
+                Admin.FirstName = "Nguyễn Phúc";
+                Admin.LastName = "Thịnh";
+                Admin.AvatarUrl = "Avatars/user1.jpg";
+                Admin.BirthDay = DateTime.Now;
+                Admin.Address = "Cao Đẳng Sài Gòn";
+                Admin.Age = 22;
+                string AdminPassword = "Admin123";
+                var resultAdmin = await _userManager.CreateAsync(Admin, AdminPassword);
+            }
+
+
+            
+            var testModerator = await _userManager.FindByNameAsync("Moderator");
+            if (testModerator != null) 
+            { 
+                Acc++; 
+                testMod = "Moderator đã được khởi tạo";
+            }
+            else
+            {
+                var Moderator = new User();
+                Moderator.UserName = "Moderator";
+                Moderator.Email = "Moderator@gmail.com";
+                Moderator.FirstName = "Huỳnh Duy";
+                Moderator.LastName = "Khánh";
+                Moderator.AvatarUrl = "Avatars/user2.jpg";
+                Moderator.BirthDay = DateTime.Now;
+                Moderator.Address = "Cao Đẳng Sài Gòn";
+                Moderator.Age = 22;
+                string ModPassword = "Mod123";
+                var resultMod = await _userManager.CreateAsync(Moderator, ModPassword);
+            }
+
+
+            
+            var testClient = await _userManager.FindByNameAsync("Client");
+            if (testClient != null) 
+            { 
+                Acc++; 
+                testCli = "Client đã được khởi tạo";
+            }
+            else
+            {
+                var Client = new User();
+                Client.UserName = "Client";
+                Client.Email = "Client@gmail.com";
+                Client.FirstName = "Mr .";
+                Client.LastName = "Gpt";
+                Client.AvatarUrl = "Avatars/user3.jpg";
+                Client.BirthDay = DateTime.Now;
+                Client.Address = "Cao Đẳng Sài Gòn";
+                Client.Age = 22;
+                string ClientPassword = "Client123";
+                var resultClient = await _userManager.CreateAsync(Client, ClientPassword);
+            }
+
+
+            return new JsonResult(new
+            {
+                AccountCreated = "Số tài khoản được tạo là" + Acc,
+                TestAd = testAd,
+                TestMod = testMod,
+                TestCli = testCli
+            }) ;
         }
     }
 }
