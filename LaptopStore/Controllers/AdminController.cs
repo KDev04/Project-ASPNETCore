a3bc461424fa257cccaf7a7ffcccbb620c413bb2
khanh <nguyenkhanhan79@gmail.com>
2023-12-26 13:10:15 +0700

    chỉnh layout , thêm admin

--- LaptopStore/Controllers/AdminController.cs
+++ LaptopStore/Controllers/AdminController.cs
@@ -6,6 +6,15 @@ namespace LaptopStore.Controllers
 {
     public class AdminController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+         public IActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
