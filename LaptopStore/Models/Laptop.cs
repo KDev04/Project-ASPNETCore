94c7efce382c35eb7c9538e287d63bf9bbfd3280
Oranty <thinhnguyenphuc51@gmail.com>
2023-12-28 13:16:01 +0700

    bo phan trang

--- LaptopStore/Models/Laptop.cs
+++ LaptopStore/Models/Laptop.cs
@@ -8,7 +8,6 @@
             public string? Type { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Quantity { get; set; } // so luong ton kho
-        public string Description { get; set; } = string.Empty;
         public string ImgPath { get; set; } = string.Empty;
         public DateTime CreateDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
