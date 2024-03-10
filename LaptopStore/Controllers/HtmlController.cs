0e28f27e265958349d28440a061e60f41b72d5d5
Dr.MoHo <nguyenkhanhan79@example.com>
2024-03-11 06:06:46 +0700

    style (admin page): làm lại giao diện quản lý

---
+++ LaptopStore/Controllers/HtmlController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+using Humanizer;
+using LaptopStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LaptopStore.Controllers
+{
+     public class HtmlController : Controller
+    {
+          public IActionResult Userpage()
+        {
+            return View();
+        }
+        public IActionResult Blankpage()
+        {
+            return View();
+        }
+    }
+}
